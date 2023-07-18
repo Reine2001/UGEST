e44724abb761d8b811a8f6ba63c6d34b923151e1
eulalie <eulaliethiombiano@gmail.com>
2023-07-18 17:08:50 +0200

    first commit

--- app/src/main/java/com/example/ugest/login.java
+++ app/src/main/java/com/example/ugest/login.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class login extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextInputEditText editText = findViewById(R.id.username);
+        editText.setText("John Doe");
     }
 }
