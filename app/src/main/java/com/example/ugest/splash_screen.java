f46e0650e437541ed9c75a6e658c20a64516feb4
Reine2001 <eulaliethiombiano@gmail.com>
2023-07-21 07:29:33 +0200

    fifth commit

---
+++ app/src/main/java/com/example/ugest/splash_screen.java
@@ -0,0 +1,54 @@
+package com.example.ugest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splash_screen extends AppCompatActivity {
+
+    //hooks
+    View first,second,third,fourth,fifth,sixth;
+    ImageView image;
+    TextView text;
+
+    //animation
+    Animation topAnimation, bottomAnimation, middleAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this,R.anim.middle_animation);
+
+        first=findViewById(R.id.first_line);
+        second=findViewById(R.id.second_line);
+        third=findViewById(R.id.third_line);
+        fourth=findViewById(R.id.fourth_line);
+        fifth=findViewById(R.id.fifth_line);
+        sixth=findViewById(R.id.sixth_line);
+
+        image=findViewById(R.id.image);
+        text=findViewById(R.id.text);
+        //setting animation
+
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+
+        image.setAnimation(middleAnimation);
+        text.setAnimation(bottomAnimation);
+    }
+}
